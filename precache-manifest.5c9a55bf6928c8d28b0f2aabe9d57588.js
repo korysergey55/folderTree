48dd34577ab922738684288c1c94a6024ef18ad0
Serhiy Korytskyy <korysergey55@gmail.com>
2021-11-12 12:54:00 +0200

    Updates

---
+++ precache-manifest.5c9a55bf6928c8d28b0f2aabe9d57588.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef16f2f78a22334820592cf3e9946fd6",
+    "url": "/folderTree/index.html"
+  },
+  {
+    "revision": "a5bbfdc8214c3b5a92e2",
+    "url": "/folderTree/static/css/2.5cc265e8.chunk.css"
+  },
+  {
+    "revision": "f4aa4d73e59fb066c1fb",
+    "url": "/folderTree/static/css/main.4636ead4.chunk.css"
+  },
+  {
+    "revision": "a5bbfdc8214c3b5a92e2",
+    "url": "/folderTree/static/js/2.712fe91e.chunk.js"
+  },
+  {
+    "revision": "3975064d7735ae6d64a08870a919e367",
+    "url": "/folderTree/static/js/2.712fe91e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4aa4d73e59fb066c1fb",
+    "url": "/folderTree/static/js/main.f714eb73.chunk.js"
+  },
+  {
+    "revision": "4db841f3dfa662f7bbb3",
+    "url": "/folderTree/static/js/runtime-main.56dc0c98.js"
+  }
+]);
